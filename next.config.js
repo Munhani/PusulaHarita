1868b08fcb9d863a1b4e6e02387977d5f1bc4964
Pusula2006 <tsivri@pusulamuhendislik.com>
2025-06-15 15:09:44 +0300

    fix: Vercel dağıtım hatalarını düzelt

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   experimental: {
-    optimizeCss: true,
+    optimizeCss: false,
     scrollRestoration: true,
   },
   compiler: {
@@ -15,6 +15,7 @@ const nextConfig = {
   },
   poweredByHeader: false,
   reactStrictMode: true,
+  output: 'standalone',
 }
 
 module.exports = nextConfig 
